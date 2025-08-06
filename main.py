55beb3439f9be43b3bdd73e7525dc9b3ec8db2e6
J4N3LA <nikajanelidze005@gmail.com>
2025-08-06 21:28:39 +0400

    Add try/except for dest_dir

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import subprocess
 import os
 import datetime
+
+
 def user_input():
+
     while True:
         dir_to_backup = os.path.expanduser(input("❔ Please enter directory path to backup >>> "))
 
@@ -11,19 +14,25 @@ def user_input():
 
 
     while True:
-        dest_dir = os.path.expanduser(input(f"❔ Please specify destionation directoy for backup archives >>> "))
+        dest_dir = os.path.expanduser(input(f"❔ Please specify destination directoy for backup archives >>> "))
 
         if not os.path.isdir(dest_dir):
-            print(f"❗ Directory \"{dest_dir}\" does not exist. Please enter correct destination directory.")
+            try:
+                print(f"❗ Directory \"{dest_dir}\" does not exist. Creating...")
+                os.makedirs(dest_dir,exist_ok=True)
+                print(f"✅ Script will backup \"{dir_to_backup}\" directory.\n✅ Archives will be saved in: \"{dest_dir}\".")
+                break
+            except Exception as e:
+                print(f"❗Could not create directory.\nERROR:\n{e}")
         else:
             print(f"✅ Script will backup \"{dir_to_backup}\" directory.\n✅ Archives will be saved in: \"{dest_dir}\".")
             break
 
     while True: 
-        algos = {"zip":False,"gzip":False,"bzip2":False}
-        algo = input("\nThis script uses 'tar' to archive specified directories. Please type the algorithm to use for compression.\nSupported algorithms are ('zip','gzip','bzip2') >>> ")
+        algos = {"xz":False,"gzip":False,"bzip2":False}
+        algo = input("\nThis script uses 'tar' to archive specified directories. Please type the algorithm to use for compression.\nSupported algorithms are ('xz','gzip','bzip2') >>> ")
         if not algo in algos:
-            print("Typed compression algorithm not available. Please type one of these: ('zip','gzip','bzip2')")
+            print("Typed compression algorithm not available. Please type one of these: ('xz','gzip','bzip2')")
         else:
             algos[algo] = True
             break
@@ -32,7 +41,8 @@ def user_input():
 
 def compress():
     date =  datetime.datetime.now().strftime("%d/%m/%Y-%H:%M")
+    subprocess.run([])
 
-    print(date)
-
-compress()
+    
+user_input()
+#compress()
