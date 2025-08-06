4a6b773bf8976acaf465acb4c0a58ab92b94c646
J4N3LA <nikajanelidze005@gmail.com>
2025-08-07 00:33:39 +0400

    build compress function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import datetime
 
 
+
 def user_input():
 
     while True:
@@ -23,26 +24,54 @@ def user_input():
                 print(f"✅ Script will backup \"{dir_to_backup}\" directory.\n✅ Archives will be saved in: \"{dest_dir}\".")
                 break
             except Exception as e:
-                print(f"❗Could not create directory.\nERROR:\n{e}")
+                print(f"❗ Could not create directory.\nERROR:\n{e}")
         else:
             print(f"✅ Script will backup \"{dir_to_backup}\" directory.\n✅ Archives will be saved in: \"{dest_dir}\".")
             break
 
     while True: 
-        algos = {"xz":False,"gzip":False,"bzip2":False}
-        algo = input("\nThis script uses 'tar' to archive specified directories. Please type the algorithm to use for compression.\nSupported algorithms are ('xz','gzip','bzip2') >>> ")
+        algos = {"xz":False,"gzip":False,"bzip2":False,"None":False}
+        algo = input("\nThis script uses 'tar' to archive specified directories. Please type the algorithm to use for compression.\nSupported algorithms are ('xz','gzip','bzip2','None) >>> ")
         if not algo in algos:
-            print("Typed compression algorithm not available. Please type one of these: ('xz','gzip','bzip2')")
+            print("Typed compression algorithm not available. Please type one of these: ('xz','gzip','bzip2','None')")
         else:
             algos[algo] = True
             break
 
-    return dir_to_backup,dest_dir,algo
+    return dir_to_backup,dest_dir,algos
 
-def compress():
-    date =  datetime.datetime.now().strftime("%d/%m/%Y-%H:%M")
-    subprocess.run([])
+def compress(src,dest,alg):
+    date =  datetime.datetime.now().strftime("%d-%m-%Y-%H:%M")
+
+    for i in alg:
+        if alg.get(i)== True:
+            compression = i
+            break
+
+    algorithms = {
+        'xz': {"ext":".tar.xz", "options":["-cJf"]},
+        'gzip': {"ext":".tar.gzip", "options":["-czf"]},
+        'bzip2': {"ext":".tar.bzip2", "options":["-cjf"]},
+        'None': {"ext":".tar", "options":["-cf"]},
+    }
+
+    compress_config = algorithms.get(compression)
+    archive_name = dest+"/backup" + "--" + date + compress_config["ext"]
+    src_target = os.path.basename(src)
+    src_dir = os.path.dirname(src)
+
+
+    # print(archive_name)
+    cmd = ['tar'] + compress_config.get("options") + [archive_name,"-C", src_dir , src_target]
+
+    print(cmd)
+
+    subprocess.run(cmd)
+
+
 
     
-user_input()
-#compress()
+
+    
+dir_to_backup,dest_dir,algos =  user_input()
+compress(dir_to_backup,dest_dir,algos)
